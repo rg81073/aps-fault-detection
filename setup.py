51826f408d314418afad85d00c00244ac5423e6d
rg81073 <2014pceevnecravi@poornima.org>
2023-07-03 14:29:51 +0530

    Data Ingestion Completed

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements()->List[str]:
 
 setup(
     name="sensor",
-    version="0.0.1",
+    version="0.0.2",
     author="ineuron",
     author_email="ravi81073@gmail.com",
     packages = find_packages(),
